98f998fe7e1386ca4e152316323f8db6b4e6e723
Chris Bautista <chrisbjr@gmail.com>
2014-04-19 17:45:59 +0800

    Added calculator to the actionbar and SSS, Philhealth, Pagibig amounts

--- Philippine Income Tax/src/main/java/com/chrisbjr/android/philippineincometax/objects/IncomeTaxCalculation.java
+++ Philippine Income Tax/src/main/java/com/chrisbjr/android/philippineincometax/objects/IncomeTaxCalculation.java
@@ -1,8 +1,6 @@
 package com.chrisbjr.android.philippineincometax.objects;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
+import android.content.Context;
 
 import com.chrisbjr.android.philippineincometax.models.NonTaxableIncomeModel;
 import com.chrisbjr.android.philippineincometax.models.PagibigModel;
@@ -10,240 +8,258 @@ import com.chrisbjr.android.philippineincometax.models.PhilHealthModel;
 import com.chrisbjr.android.philippineincometax.models.SssModel;
 import com.chrisbjr.android.philippineincometax.models.TaxableIncomeModel;
 import com.chrisbjr.android.philippineincometax.models.WithholdingTaxModel;
-import android.content.Context;
-
 import com.j256.ormlite.field.DatabaseField;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 public class IncomeTaxCalculation {
 
-	@DatabaseField(generatedId = true)
-	private int id;
-	@DatabaseField
-	private double salaryRate;
-	@DatabaseField
-	private int salaryFrequency;
-	@DatabaseField
-	private int withholdingTaxTypeId;
-	@DatabaseField
-	private int dependents;
-	@DatabaseField
-	private boolean isSssActive;
-	@DatabaseField
-	private boolean isPhilhealthActive;
-	@DatabaseField
-	private boolean isPagibigActive;
-	@DatabaseField
-	private Date createdOn;
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private double salaryRate;
+    @DatabaseField
+    private int salaryFrequency;
+    @DatabaseField
+    private int withholdingTaxTypeId;
+    @DatabaseField
+    private int dependents;
+    @DatabaseField
+    private boolean isSssActive;
+    @DatabaseField
+    private boolean isPhilhealthActive;
+    @DatabaseField
+    private boolean isPagibigActive;
+    @DatabaseField
+    private Date createdOn;
 
-	public IncomeTaxCalculation() {
-		// Nothing here
-	}
+    private Sss mSss = null;
+    private Philhealth mPhilhealth = null;
+    private Pagibig mPagibig = null;
 
-	public IncomeTaxCalculation(double salaryRate) {
-		this.salaryRate = salaryRate;
-		this.salaryFrequency = 1;
-		this.withholdingTaxTypeId = 2;
-		this.dependents = 0;
-		this.isSssActive = true;
-		this.isPhilhealthActive = true;
-		this.isPagibigActive = true;
-		this.createdOn = new Date();
-	}
+    public IncomeTaxCalculation() {
+        // Nothing here
+    }
 
-	public int getId() {
-		return id;
-	}
+    public IncomeTaxCalculation(double salaryRate) {
+        this.salaryRate = salaryRate;
+        this.salaryFrequency = 1;
+        this.withholdingTaxTypeId = 2;
+        this.dependents = 0;
+        this.isSssActive = true;
+        this.isPhilhealthActive = true;
+        this.isPagibigActive = true;
+        this.createdOn = new Date();
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public double getSalaryRate() {
-		return salaryRate;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setSalaryRate(double salaryRate) {
-		this.salaryRate = salaryRate;
-	}
+    public double getSalaryRate() {
+        return salaryRate;
+    }
 
-	public int getSalaryFrequency() {
-		return salaryFrequency;
-	}
+    public void setSalaryRate(double salaryRate, Context ctx) {
+        this.salaryRate = salaryRate;
+        setSss(ctx);
+        setPhilhealth(ctx);
+        setPagibig(ctx);
+    }
 
-	public String getSalaryFrequencyString() {
-		switch (getSalaryFrequency()) {
-		case 1:
-			return "MONTHLY";
-		case 2:
-			return "SEMI-MONTHLY";
-		case 4:
-			return "WEEKLY";
-		case 20:
-			return "DAILY";
-		default:
-			return "MONTHLY";
-		}
-	}
+    private void setSss(Context ctx) {
+        if (isSssActive()) {
+            SssModel sssModel = new SssModel(ctx);
+            mSss = sssModel.getSss(getSalaryRate(), getSalaryFrequency());
+        } else {
+            mSss = null;
+        }
+    }
 
-	public void setSalaryFrequency(int salaryFrequency) {
-		this.salaryFrequency = salaryFrequency;
-	}
+    private void setPhilhealth(Context ctx) {
+        if (isPhilhealthActive()) {
+            PhilHealthModel phModel = new PhilHealthModel(ctx);
+            mPhilhealth = phModel.getPhilhealth(getSalaryRate(),
+                    getSalaryFrequency());
+        } else {
+            mPhilhealth = null;
+        }
+    }
 
-	public int getWithholdingTaxTypeId() {
-		return withholdingTaxTypeId;
-	}
+    private void setPagibig(Context ctx) {
+        if (isPagibigActive()) {
+            PagibigModel pModel = new PagibigModel(ctx);
+            mPagibig = pModel
+                    .getPagibig(getSalaryRate(), getSalaryFrequency());
+        } else {
+            mPagibig = null;
+        }
+    }
 
-	public void setWithholdingTaxTypeId(int withholdingTaxTypeId) {
-		this.withholdingTaxTypeId = withholdingTaxTypeId;
-	}
+    public Sss getSss() {
+        return mSss;
+    }
 
-	public int getDependents() {
-		return dependents;
-	}
+    public Philhealth getPhilhealth() {
+        return mPhilhealth;
+    }
 
-	public void setDependents(int dependents) {
-		this.dependents = dependents;
-		if (dependents > 4) {
-			this.dependents = 4;
-		}
-		if (dependents < 0) {
-			this.dependents = 0;
-		}
-	}
+    public Pagibig getPagibig() {
+        return mPagibig;
+    }
 
-	public boolean isSssActive() {
-		return isSssActive;
-	}
+    public int getSalaryFrequency() {
+        return salaryFrequency;
+    }
 
-	public void setSssActive(boolean isSssActive) {
-		this.isSssActive = isSssActive;
-	}
+    public String getSalaryFrequencyString() {
+        switch (getSalaryFrequency()) {
+            case 1:
+                return "MONTHLY";
+            case 2:
+                return "SEMI-MONTHLY";
+            case 4:
+                return "WEEKLY";
+            case 20:
+                return "DAILY";
+            default:
+                return "MONTHLY";
+        }
+    }
 
-	public boolean isPhilhealthActive() {
-		return isPhilhealthActive;
-	}
+    public void setSalaryFrequency(int salaryFrequency) {
+        this.salaryFrequency = salaryFrequency;
+    }
 
-	public void setPhilhealthActive(boolean isPhilhealthActive) {
-		this.isPhilhealthActive = isPhilhealthActive;
-	}
+    public int getWithholdingTaxTypeId() {
+        return withholdingTaxTypeId;
+    }
 
-	public boolean isPagibigActive() {
-		return isPagibigActive;
-	}
+    public void setWithholdingTaxTypeId(int withholdingTaxTypeId) {
+        this.withholdingTaxTypeId = withholdingTaxTypeId;
+    }
 
-	public void setPagibigActive(boolean isPagibigActive) {
-		this.isPagibigActive = isPagibigActive;
-	}
+    public int getDependents() {
+        return dependents;
+    }
 
-	public Sss getSss(Context ctx) {
-		if (isSssActive()) {
-			SssModel sssModel = new SssModel(ctx);
-			Sss sss = sssModel.getSss(getSalaryRate(), getSalaryFrequency());
-			return sss;
-		}
-		return null;
-	}
+    public void setDependents(int dependents) {
+        this.dependents = dependents;
+        if (dependents > 4) {
+            this.dependents = 4;
+        }
+        if (dependents < 0) {
+            this.dependents = 0;
+        }
+    }
 
-	public Philhealth getPhilhealth(Context ctx) {
-		if (isPhilhealthActive()) {
-			PhilHealthModel phModel = new PhilHealthModel(ctx);
-			Philhealth ph = phModel.getPhilhealth(getSalaryRate(),
-					getSalaryFrequency());
-			return ph;
-		}
-		return null;
-	}
+    public boolean isSssActive() {
+        return isSssActive;
+    }
 
-	public Pagibig getPagibig(Context ctx) {
-		if (isPagibigActive()) {
-			PagibigModel pModel = new PagibigModel(ctx);
-			Pagibig p = pModel
-					.getPagibig(getSalaryRate(), getSalaryFrequency());
-			return p;
-		}
-		return null;
-	}
+    public void setSssActive(boolean isSssActive) {
+        this.isSssActive = isSssActive;
+    }
 
-	public List<TaxableIncome> getTaxableIncome(Context ctx) {
-		TaxableIncomeModel taxableIncomeModel = new TaxableIncomeModel(ctx);
-		return taxableIncomeModel.getListFromIncomeTaxCalculation(this);
-	}
+    public boolean isPhilhealthActive() {
+        return isPhilhealthActive;
+    }
 
-	public List<NonTaxableIncome> getNonTaxableIncome(Context ctx) {
-		NonTaxableIncomeModel nonTaxableIncomeModel = new NonTaxableIncomeModel(
-				ctx);
-		return nonTaxableIncomeModel.getListFromIncomeTaxCalculation(this);
-	}
+    public void setPhilhealthActive(boolean isPhilhealthActive) {
+        this.isPhilhealthActive = isPhilhealthActive;
+    }
 
-	public double getTotalTaxableIncomeAmount(Context ctx) {
-		Sss sss = getSss(ctx);
-		Philhealth philhealth = getPhilhealth(ctx);
-		Pagibig pagibig = getPagibig(ctx);
+    public boolean isPagibigActive() {
+        return isPagibigActive;
+    }
 
-		double taxableIncome = getSalaryRate();
+    public void setPagibigActive(boolean isPagibigActive) {
+        this.isPagibigActive = isPagibigActive;
+    }
 
-		if (sss != null) {
-			taxableIncome = taxableIncome - sss.getEmployeeContribution();
-		}
-		if (philhealth != null) {
-			taxableIncome = taxableIncome
-					- philhealth.getEmployeeContribution();
-		}
-		if (pagibig != null) {
-			taxableIncome = taxableIncome - pagibig.getEmployeeContribution();
-		}
 
-		double taxableIncomeListAmount = 0;
-		List<TaxableIncome> taxableIncomeList = getTaxableIncome(ctx);
-		if (taxableIncomeList != null && taxableIncomeList.size() > 0) {
-			for (TaxableIncome t : taxableIncomeList) {
-				taxableIncomeListAmount = taxableIncomeListAmount
-						+ t.getAmount();
-			}
-		}
+    public List<TaxableIncome> getTaxableIncome(Context ctx) {
+        TaxableIncomeModel taxableIncomeModel = new TaxableIncomeModel(ctx);
+        return taxableIncomeModel.getListFromIncomeTaxCalculation(this);
+    }
 
-		taxableIncome = taxableIncome + taxableIncomeListAmount;
-		System.out.println("taxableincomelistamount is: "
-				+ taxableIncomeListAmount);
-		System.out.println("taxable income is: " + taxableIncome);
+    public List<NonTaxableIncome> getNonTaxableIncome(Context ctx) {
+        NonTaxableIncomeModel nonTaxableIncomeModel = new NonTaxableIncomeModel(
+                ctx);
+        return nonTaxableIncomeModel.getListFromIncomeTaxCalculation(this);
+    }
 
-		return taxableIncome;
-	}
+    public double getTotalTaxableIncomeAmount(Context ctx) {
+        double taxableIncome = getSalaryRate();
 
-	public WithholdingTax getWithholdingTax(Context ctx) {
-		WithholdingTaxModel wwModel = new WithholdingTaxModel(ctx);
-		WithholdingTax withholdingTax = wwModel.getWithholdingTax(
-				getTotalTaxableIncomeAmount(ctx), getDependents(),
-				getWithholdingTaxTypeId(), getSalaryFrequencyString());
-		return withholdingTax;
-	}
+        if (mSss != null) {
+            taxableIncome = taxableIncome - mSss.getEmployeeContribution();
+        }
+        if (mPhilhealth != null) {
+            taxableIncome = taxableIncome
+                    - mPhilhealth.getEmployeeContribution();
+        }
+        if (mPagibig != null) {
+            taxableIncome = taxableIncome - mPagibig.getEmployeeContribution();
+        }
 
-	public double getNetIncome(Context ctx) {
-		double netIncome = getTotalTaxableIncomeAmount(ctx)
-				- getWithholdingTax(ctx).getWithholdingTaxAmount();
+        double taxableIncomeListAmount = 0;
+        List<TaxableIncome> taxableIncomeList = getTaxableIncome(ctx);
+        if (taxableIncomeList != null && taxableIncomeList.size() > 0) {
+            for (TaxableIncome t : taxableIncomeList) {
+                taxableIncomeListAmount = taxableIncomeListAmount
+                        + t.getAmount();
+            }
+        }
 
-		List<NonTaxableIncome> nonTaxableIncomeList = getNonTaxableIncome(ctx);
-		double nonTaxableIncomeAmount = 0;
-		if (nonTaxableIncomeList != null && nonTaxableIncomeList.size() > 0) {
-			for (NonTaxableIncome n : nonTaxableIncomeList) {
-				nonTaxableIncomeAmount = nonTaxableIncomeAmount + n.getAmount();
-			}
-			System.out.println("non taxable income amoun is: "
-					+ nonTaxableIncomeAmount);
-		}
+        taxableIncome = taxableIncome + taxableIncomeListAmount;
+        System.out.println("taxableincomelistamount is: "
+                + taxableIncomeListAmount);
+        System.out.println("taxable income is: " + taxableIncome);
 
-		netIncome = netIncome + nonTaxableIncomeAmount;
+        return taxableIncome;
+    }
 
-		return round(netIncome, 2);
-	}
+    public WithholdingTax getWithholdingTax(Context ctx) {
+        WithholdingTaxModel wwModel = new WithholdingTaxModel(ctx);
+        WithholdingTax withholdingTax = wwModel.getWithholdingTax(
+                getTotalTaxableIncomeAmount(ctx), getDependents(),
+                getWithholdingTaxTypeId(), getSalaryFrequencyString());
+        return withholdingTax;
+    }
 
-	private double round(double value, int places) {
-		if (places < 0)
-			throw new IllegalArgumentException();
+    public double getNetIncome(Context ctx) {
+        double netIncome = getTotalTaxableIncomeAmount(ctx)
+                - getWithholdingTax(ctx).getWithholdingTaxAmount();
 
-		BigDecimal bd = new BigDecimal(value);
-		bd = bd.setScale(places, BigDecimal.ROUND_HALF_UP);
-		return Double.parseDouble(bd.toString());
-	}
+        List<NonTaxableIncome> nonTaxableIncomeList = getNonTaxableIncome(ctx);
+        double nonTaxableIncomeAmount = 0;
+        if (nonTaxableIncomeList != null && nonTaxableIncomeList.size() > 0) {
+            for (NonTaxableIncome n : nonTaxableIncomeList) {
+                nonTaxableIncomeAmount = nonTaxableIncomeAmount + n.getAmount();
+            }
+            System.out.println("non taxable income amoun is: "
+                    + nonTaxableIncomeAmount);
+        }
+
+        netIncome = netIncome + nonTaxableIncomeAmount;
+
+        return round(netIncome, 2);
+    }
+
+    private double round(double value, int places) {
+        if (places < 0)
+            throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, BigDecimal.ROUND_HALF_UP);
+        return Double.parseDouble(bd.toString());
+    }
 
 }
